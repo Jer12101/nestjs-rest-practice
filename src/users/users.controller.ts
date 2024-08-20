c733d2aad0629cadee980b7ec1c0e80987799bba
Jer12101 <jerryhsieh0121+github@gmail.com>
2024-08-20 17:15:10 +0800

    feat: added UsersService for the functions in the controller route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Param, Post, Body, Patch, Delete, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users') 
 export class UsersController {
+    constructor(private readonly usersService: UsersService){}
     /*
     GET /users
     GET /users/:id
@@ -11,8 +13,8 @@ export class UsersController {
     */
 
     @Get() // GET /users route
-    findAll(@Query('role') role?: 'INTERN' | 'CLIENT' | 'ADMIN') {
-        return [];
+    findAll(@Query('role') role?: 'SALES' | 'CLIENT' | 'ADMIN') {
+        return this.usersService.findAll(role);
     }
 
     /*@Get('interns') // this cannot be routed behind :id
@@ -22,21 +24,21 @@ export class UsersController {
 
     @Get(':id') // GET /users/:id
     findOne(@Param('id') id: string) {
-        return { id };
+        return this.usersService.findOne(+id);
     }
 
     @Post()
-    create(@Body() user: {}) {
-        return {user};
+    create(@Body() user: {name: string, email: string, role: 'SALES' | 'CLIENT' | 'ADMIN'}) {
+        return this.usersService.create(user);
     }
 
     @Patch(':id') // GET /users/:id
-    udpate(@Param('id') id: string, @Body() userUpdate: {}) {
-        return { id, ...userUpdate };
+    udpate(@Param('id') id: string, @Body() userUpdate: {name?: string, email?: string, role?: 'SALES' | 'CLIENT' | 'ADMIN'}) {
+        return this.usersService.udpate(+id, userUpdate);
     }
 
     @Delete(':id') // GET /users/:id
     delete(@Param('id') id: string) {
-        return { id };
+        return this.usersService.delete(+id);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,75 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [ // static data
+        {
+            "id": 1,
+            "name": "Jim Halpert",
+            "email": "jh@dunder-mifflin.com",
+            "role": "SALES"
+        },
+        {
+            "id": 2,
+            "name": "Michael Scott",
+            "email": "MS@dunder-mifflin.com",
+            "role": "ADMIN"
+        },
+        {
+            "id": 3,
+            "name": "Dwight Schrute",
+            "email": "ds@dunder-mifflin.com",
+            "role": "SALES"
+        },
+        {
+            "id": 4,
+            "name": "John Doe",
+            "email": "jd@clients.com",
+            "role": "CLIENT"
+        },
+        {
+            "id": 5,
+            "name": "John Doe Sr.",
+            "email": "jds@dunder-mifflin.com",
+            "role": "CLIENT"
+        },
+    ]
+
+    findAll(role?: 'SALES' | 'CLIENT' | 'ADMIN'){
+        if (role) {
+            return this.users.filter(user => user.role === role);
+        }
+        return this.users;
+    }
+
+    findOne(id: number) {
+        const user = this.users.find(user => user.id === id);
+        return user;
+    }
+
+    create(user: {name: string, email: string, role: 'SALES' | 'CLIENT' | 'ADMIN'}) {
+        const usersByHighestID = [...this.users].sort((a, b) => b.id - a.id)
+        const newUser = {
+            id: usersByHighestID[0].id + 1, 
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    udpate(id: number, updatedUser: {name?: string, email?: string, role?: 'SALES' | 'CLIENT' | 'ADMIN'}) {
+        this.users = this.users.map(user => {
+            if(user.id === id) {
+                return {...user, ...updatedUser}
+            }
+            return user
+        })
+        return this.findOne(id)
+    }
+
+    delete(id: number) {
+        const removedUser = this.findOne(id)
+        this.users = this.users.filter(user => user.id !== id)
+        return removedUser
+    }
+}
